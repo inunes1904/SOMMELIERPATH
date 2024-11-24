e100a5e55be3e6d980665e03598a881e3443aa6a
Ivo Nunes <inunes1904@gmail.com>
2024-11-24 23:41:22 +0000

    APP JS API description

--- app.js
+++ app.js
@@ -22,17 +22,52 @@ mongoose.connect(DB_URI_FINAL, {
   .catch(err => console.error('Database connection error:', err));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to SOMMPATH API!');
+  res.send(
+    '<h1>WELCOME TO SOMMELIER PATH API</h1>\n' +
+    '<p>ROUTES:</p>\n' +
+    '\n' +
+    '<ul>\n' +
+    '  <li><a href="/users">GET /users</a> - Fetch a list of all users</li>\n' +
+    '  <li><a href="/users/:id">GET /users/:id</a> - Fetch details of a specific user</li>\n' +
+    '  <li><a href="/users">POST /users</a> - Create a new user</li>\n' +
+    '  <li><a href="/users/:id">PUT /users/:id</a> - Update a user\'s details</li>\n' +
+    '  <li><a href="/users/:id">DELETE /users/:id</a> - Delete a user</li>\n' +
+    '</ul>\n' +
+    '\n' +
+    '<ul>\n' +
+    '  <li><a href="/configuracao">GET /configuracoes</a> - Fetch a list of all wine-tasting configurations</li>\n' +
+    '  <li><a href="/configuracao/:id">GET /configuracoes/:id</a> - Fetch details of a specific wine-tasting configuration</li>\n' +
+    '  <li><a href="/configuracao">POST /configuracoes</a> - Create a new wine-tasting configuration</li>\n' +
+    '  <li><a href="/configuracao/:id">PUT /configuracoes/:id</a> - Update a wine-tasting configuration</li>\n' +
+    '  <li><a href="/configuracao/:id">DELETE /configuracoes/:id</a> - Delete a wine-tasting configuration</li>\n' +
+    '</ul>' +
+    '<ul>' +
+    '<li><a href="/avaliacao">GET /avaliacoes</a> - Fetch a list of all evaluations</li>' +
+    '<li><a href="/avaliacao/:id">GET /avaliacoes/:id</a> - Fetch details of a specific evaluation</li>' +
+    '<li><a href="/avaliacao">POST /avaliacoes</a> - Create a new evaluation</li>' +
+    '<li><a href="/avaliacao/:id">PUT /avaliacoes/:id</a> - Update an evaluation</li>' +
+    '<li><a href="/avaliacao/:id">DELETE /avaliacoes/:id</a> - Delete an evaluation</li>' +
+    '</ul>' +
+    '<ul>'+
+      '<li><a href="/users">GET /users</a> - Fetch a list of all users</li>'+
+      '<li><a href="/users/:id">GET /users/:id</a> - Fetch details of a specific user</li>'+
+      '<li><a href="/users">POST /users</a> - Create a new user</li>'+
+     ' <li><a href="/users/:id">PUT /users/:id</a> - Update a user role (admin only)</li>'+
+      '<li><a href="/users/:id">DELETE /users/:id</a> - Delete a user (admin only)</li>'+
+
+      '<li><a href="/login">POST /login</a> - User login to authenticate and receive a token</li>'+
+      '<li><a href="/user">GET /user</a> - Retrieve user information (requires valid token)</li>'+
+    '</ul>'
+  );
 });
 
-
 // Routes
-app.use( `${BASE_URL}/users`, userRoutes);
-app.use( `${BASE_URL}`, authRoutes);
-app.use( `${BASE_URL}/configuracao`, configuracaoRoutes);
+app.use(`${BASE_URL}/users`, userRoutes);
+app.use(`${BASE_URL}`, authRoutes);
+app.use(`${BASE_URL}/configuracao`, configuracaoRoutes);
 
 // Start server
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running http://localhost:${PORT}`);
+  console.log(`Server is running http://localhost:${PORT}`);
 });
