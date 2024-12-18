1505d70ab50abaaa6a94196e9d997e42919178d3
Ivo Nunes <inunes1904@gmail.com>
2024-12-18 22:30:35 +0000

    starting modifications

--- routes/configuracaoRoutes.js
+++ routes/configuracaoRoutes.js
@@ -6,13 +6,13 @@ const { verifyToken } = require("../utils/authUtils");
 const router = express.Router();
 
 // Ensure token verification happens before any role checking or controller logic
-router.use(verifyToken);
+// router.use(verifyToken);
 
-// Define routes with role-based access
-router.post('/', hasRole('sommelier', 'admin'), configuracaoController.create);
-router.get('/', hasRole('sommelier', 'admin', 'user'), configuracaoController.getAll);
-router.get('/:id', hasRole('sommelier', 'admin', 'user'), configuracaoController.getById);
-router.put('/:id', hasRole('sommelier', 'admin'), configuracaoController.update);
-router.delete('/:id', hasRole('sommelier', 'admin'), configuracaoController.delete);
+// Define routes with role-based access hasRole('sommelier', 'admin'),
+router.post('/', configuracaoController.create);
+router.get('/', configuracaoController.getAll);
+router.get('/:id', configuracaoController.getById);
+router.put('/:id', configuracaoController.update);
+router.delete('/:id', configuracaoController.delete);
 
 module.exports = router;
