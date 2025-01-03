38507a5bb2f9f6ccda313a4d49bcf611615be669
Ivo Nunes <inunes1904@gmail.com>
2025-01-03 18:13:24 +0000

    Users availability done!

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,8 +5,8 @@ const {verifyToken} = require("../utils/authUtils");
 
 const router = express.Router();
 
-router.get('/users', verifyToken, hasRole('admin'), userController.getAllUsers);
-router.get('/users/:id',verifyToken,  hasRole('admin'), userController.getUserById);
+router.get('/users', userController.getAllUsers);
+router.get('/users/:id', userController.getUserById);
 router.post('/users', userController.createUser);
 router.put('/users/:id', verifyToken,  userController.updateUser);
 router.delete('/users/:id', verifyToken, hasRole('admin'), userController.deleteUser);
